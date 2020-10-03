4c0d821861231ec59e3b41deabd60dfd195fd19f
Sanat-Jha <69027091+Sanat-Jha@users.noreply.github.com>
2020-10-03 12:11:31 +0530

    Comments added
    See comments are really important as other people will see and try to understand the code so it is really important.

--- RfidReader.py
+++ RfidReader.py
@@ -1,14 +1,14 @@
-from BarCode import BarCode
-from Rfid import Rfid
+from BarCode import BarCode                 #Importing barcodemodule
+from Rfid import Rfid                 #Importing Rfid module
 
-def main():
+def main():                     #Defining the main function
     while True:
-        reader= Rfid()
-        barcodeStore= BarCode()
+        reader= Rfid()         #initiate
+        barcodeStore= BarCode()  #initiate
         reader.read()
         barcodeStore.add_product()
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__":     #code will only run in main file
+    main()      #calling the main function
 
 
